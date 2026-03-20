d8665ee85bc03deefa6cb3b3b7001238d5503ef3
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-20 15:05:52 +0530

    Implement magic packet creation for Wake-on-LAN

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -21,5 +22,25 @@ func main() {
 		return
 	}
 
-	fmt.Println("Parsed MAC Address:", mac)
+	// Create the magic packet
+	magicPacket := makeMagicPacket(mac)
+
+	fmt.Printf("Magic Packet: %x\n", magicPacket)
+}
+
+// makeMagicPacket creates a Wake-on-LAN magic packet for the given hardware address.
+// The magic packet consists of 6 bytes of 0xFF followed by 16 repetitions of the target MAC address.
+// The resulting byte slice can be sent over the network to wake up the target device.
+func makeMagicPacket(hardwareAddress net.HardwareAddr) []byte {
+	var packet bytes.Buffer
+
+	// 6 bytes of 0xFF
+	packet.Write(bytes.Repeat([]byte{0xFF}, 6))
+
+	// 16 repetitions of the MAC address
+	for i := 0; i < 16; i++ {
+		packet.Write(hardwareAddress)
+	}
+
+	return packet.Bytes()
 }
