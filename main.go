36554c017fe1ab1a82cf1e7226e9e5358e1682a5
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-03-20 15:02:28 +0530

    Read command-line arguments and parse MAC Address

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: wake <mac_address>")
+		return
+	}
+
+	macAddress := os.Args[1]
+
+	// Parse the MAC address
+	mac, err := net.ParseMAC(macAddress)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing MAC Addresss [%s]: %v\n", macAddress, err)
+		return
+	}
+
+	fmt.Println("Parsed MAC Address:", mac)
 }
